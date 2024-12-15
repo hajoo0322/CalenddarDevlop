7f0eb0263624cff6b7c170084b67d9bfe7ea244f
afkorigin <gnstldgk@naver.com>
2024-12-15 15:51:20 +0900

    예외 추가

---
+++ lv4/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.example.demo.lv4.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(FilterLoginException.class)
+    public ResponseEntity<String> handleLoginException(FilterLoginException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+}
